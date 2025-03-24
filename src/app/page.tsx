d8491ebb9a64cd69bd6ee384fd0bf6f559e3f56f
Akasukpe Aghoghomena <aghoghomena3@gmail.com>
2025-03-24 18:07:25 -0400

    Base template for Frontend Logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,177 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [isAttacking, setIsAttacking] = useState(true);
+  const [selectedImage, setSelectedImage] = useState(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+  return (
+    <div className="py-10">
+      <main>
+        <h1 className="text-4xl text-center underline mb-5">
+          Adversarial Attack on Face Recognition
+        </h1>
+        <form action="#" method="POST" className="flex flex-col gap-2">
+          <div className="flex justify-center items-center gap-20">
+            <div className="left flex flex-col gap-6">
+              {isAttacking && (
+                <div className="flex flex-col">
+                  <label htmlFor="epsilon">Enter value for ε(0-1):</label>
+                  <input
+                    type="number"
+                    name="epsilon"
+                    id="epsilon"
+                    className="border border-black rounded-sm p-2"
+                    defaultValue={0.5}
+                    min={0}
+                    max={1}
+                    step={0.01}
+                    onChange={(e) => {
+                      const value = parseFloat(e.target.value);
+                      if (value < 0 || value > 1) {
+                        alert("Error: Value must be between 0 and 1");
+                      }
+                    }}
+                  />
+                </div>
+              )}
+
+              <div className="image">
+                <label htmlFor="image" className="cursor-pointer rounded-sm">
+                  {selectedImage ? (
+                    <div className="flex flex-col border border-2 hover:bg-gray-100 items-center">
+                      {selectedImage != null && (
+                        <Image
+                          src={URL.createObjectURL(selectedImage)}
+                          alt="Selected Image"
+                          className="h-50 object-cover"
+                          width={220}
+                          height={40}
+                        />
+                      )}
+
+                      <div className="flex">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          height="24px"
+                          viewBox="0 -960 960 960"
+                          width="24px"
+                          fill="#00000"
+                        >
+                          <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
+                        </svg>
+                        <span>Change Image</span>
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="flex flex-col border border-2 py-2 hover:bg-gray-100 items-center">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="60px"
+                        viewBox="0 -960 960 960"
+                        width="60px"
+                        fill="#00000"
+                      >
+                        <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
+                      </svg>
+                      <span>Add Image</span>
+                    </div>
+                  )}
+                </label>
+
+                <input
+                  type="file"
+                  className="hidden"
+                  name="image"
+                  id="image"
+                  onChange={(e: any) => setSelectedImage(e.target.files[0])}
+                />
+              </div>
+
+              {/* RADIO BUTTONS FOR ATTACK TYPES */}
+              <div className="flex flex-col gap-2 px-5 py-5 border border-black rounded-sm">
+                <p>Attacking:</p>
+                <div className="space-x-2">
+                  <input
+                    type="radio"
+                    name="attacking"
+                    id="attack-yes"
+                    value="Yes"
+                    checked={isAttacking}
+                    onChange={() => setIsAttacking(true)}
+                  />
+                  <label htmlFor="attack-yes">Yes</label>
+                </div>
+                <div className="space-x-2">
+                  <input
+                    type="radio"
+                    name="attacking"
+                    id="attack-no"
+                    value="No"
+                    checked={!isAttacking}
+                    onChange={() => setIsAttacking(false)}
+                  />
+                  <label htmlFor="attack-no">No</label>
+                </div>
+
+                {isAttacking && (
+                  <div>
+                    <p>Attack Type:</p>
+                    <div className="space-x-2">
+                      <input
+                        type="radio"
+                        name="attacktype"
+                        id="fgsm"
+                        value="fgsm"
+                      />
+                      <label htmlFor="fgsm">FGSM</label>
+                    </div>
+                    <div className="space-x-2">
+                      <input
+                        type="radio"
+                        name="attacktype"
+                        id="pgd"
+                        value="pgd"
+                      />
+                      <label htmlFor="pgd">PDG</label>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+
+            {/* RIGHT SIDE */}
+            <div className="flex flex-col gap-4">
+              {isAttacking && (
+                <div>
+                  <p className="underline">Noise Generated</p>
+                  <div className="border border-black w-40 h-40">&nbsp;</div>
+                </div>
+              )}
+
+              <div>
+                <p className="underline">Final Image</p>
+                <div className="border border-black w-60 h-60">
+                  {selectedImage != null && (
+                    <Image
+                      src={URL.createObjectURL(selectedImage)}
+                      alt="Selected Image"
+                      className="h-full w-full object-cover"
+                      width={220}
+                      height={40}
+                    />
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <button className="bg-yellow-400 mt-5 cursor-pointer self-center text-black py-3 px-20 hover:outline hover:outline-2 outline-offset-2 outline-yellow-500">
+            Make Inference
+          </button>
+        </form>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
